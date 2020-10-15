e9ff9919da222fbb3574cc94aa8bc4dc176c550d
irfan5d7 <irfanahmed.511.ia@gmail.com>
2020-10-15 15:09:48 +0530

    Day 15

---
+++ DailyChallenges/October/Day-15-Rotate array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void rev(vector<int>& nums,int start,int end){
+        int i = start, j = end-1,temp;
+        while(i < j){
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;j--;
+        }
+    }
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        if (n ==0) return;
+        k = k%n;
+        if (k ==0) return;
+        rev(nums,0,n);
+        rev(nums,0,k);
+        rev(nums,k,n);
+    }
+};
